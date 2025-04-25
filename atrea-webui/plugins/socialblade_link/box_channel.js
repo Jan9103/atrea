93798f6054145f14d3a39c9842e0a24ded3c5d12
Shae <jan9103.wargear@protonmail.com>
2025-04-25 20:16:59 +0200

    feat(webui): add socialblade_link plugin

---
+++ atrea-webui/plugins/socialblade_link/box_channel.js
@@ -0,0 +1,16 @@
+import {g,r,t} from "./libs/xeact.js";
+const n=(e)=>document.createElement(e);
+
+r(()=>{
+  let params=new URLSearchParams(window.location.search);
+  let login_name=params.get("login");
+  let a_node=n("a");
+  a_node.setAttribute("rel", "noopener noreferrer");
+  a_node.setAttribute("target", "_blank");
+  a_node.setAttribute("href", "https://socialblade.com/twitch/user/"+login_name);
+  let img_node=n("img");
+  img_node.setAttribute("src", "https://socialblade.com/favicon-128.png");
+  img_node.setAttribute("height", "30px");
+  a_node.appendChild(img_node);
+  g("cvi_twitch_link").parentNode.appendChild(a_node);
+});
